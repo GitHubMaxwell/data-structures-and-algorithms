7b14ad97a3f8214e801c9aef780e1503cd2930fa
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-05-20 23:11:56 -0700

    finished whiteboard challenge 02

---
+++ arrays_test/array_shift.test.js
@@ -0,0 +1,17 @@
+let insertShiftArray = require('../arrays/array_shift.js');
+
+describe('Array_Shift Module', () => {
+
+  it('should return same array TEST 1', () => {
+    expect(insertShiftArray([1,2,3], 2)).toEqual([1,2,2,3]);
+  });
+
+  it('should return same array TEST 2', () => {
+    expect(insertShiftArray([1,1,1,1,1], 2)).toEqual([1,1,1,2,1,1]);
+  });
+
+  it('should return same array TEST 3', () => {
+    expect(insertShiftArray([1,2,3,4,5,6,7,8], 2)).toEqual([1,2,3,4,2,5,6,7,8]);
+  });
+
+});
