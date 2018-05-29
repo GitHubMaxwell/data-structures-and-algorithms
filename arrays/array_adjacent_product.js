296b4a0b38ac860d4a4fa1dc14a579aef02a424e
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-05-28 20:31:41 -0700

    array_adjacent_product works

--- arrays/array_adjacent_product.js
+++ arrays/array_adjacent_product.js
@@ -1,21 +1,16 @@
 'use strict';
 
 function LargestProduct(matrix) {
-  let lgProd =0;
+  let lgProd = 0;
   const maxColumn = matrix[0].length-1;
-  console.log(maxColumn);
   const maxRow = matrix.length-1;
-  console.log(maxRow);
+
 
   for(let row = 0; row <= maxRow; row++){
     for(let column = 0; column <= maxColumn; column++){
       let val = matrix[row][column];
       let product = 0;
 
-      if(hasNeighborToLeft(column,maxColumn)) {
-        product = val * matrix[row][column - 1];
-        lgProd = Math.max(lgProd, product);
-      }
       if(hasNeighborToRight(column,maxColumn)) {
         product = val * matrix[row][column +1];
         lgProd = Math.max(lgProd, product);
@@ -25,14 +20,14 @@ function LargestProduct(matrix) {
         lgProd = Math.max(lgProd, product);
       }
 
-    //   if(hasNeighborToBottom(row,maxRow) && hasNeighborToLeft(column,maxColumn)) {
-    //     product = val * matrix[row + 1][column - 1];
-    //     lgProd = Math.max(lgProd, product);
-    //   }
-    //   if(hasNeighborToBottom(row,maxRow) && hasNeighborToRight(column,maxColumn)) {
-    //     product = val * matrix[row + 1][column + 1];
-    //     lgProd = Math.max(lgProd, product);
-    //   }
+      if(hasNeighborToBottom(row,maxRow) && hasNeighborToLeft(column,maxColumn)) {
+        product = val * matrix[row + 1][column - 1];
+        lgProd = Math.max(lgProd, product);
+      }
+      if(hasNeighborToBottom(row,maxRow) && hasNeighborToRight(column,maxColumn)) {
+        product = val * matrix[row + 1][column + 1];
+        lgProd = Math.max(lgProd, product);
+      }
     }
   }
   return lgProd;
@@ -41,23 +36,23 @@ module.exports = LargestProduct;
 
 
 function hasNeighborToRight(col, newCol) {
-  if(col + 1 === newCol) {
+  if(col !== newCol) {
     return true;
   } else {
     return false;
   }
 }
-function hasNeighborToLeft(col, newCol) {
-  if(col - 1 === newCol) {
-    return true;
-  } else {
+function hasNeighborToLeft(col) {
+  if(col -1 === -1) {
     return false;
+  } else {
+    return true;
   }
 }
 function hasNeighborToBottom(row, newRow) {
-  if(row + 1 === newRow) {
-    return true;
-  } else {
+  if(row + 1 === newRow + 1) {
     return false;
+  } else {
+    return true;
   }
 }
