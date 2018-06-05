2321b3a2921c291360288b8607daf4fbf29fd2bf
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-06-04 23:17:17 -0700

    working out how to add and search for the node counter

--- ll_test/ll_kth_from_end.test.js
+++ ll_test/ll_kth_from_end.test.js
@@ -3,8 +3,22 @@ let linkedList = require('../ll/ll_kth_from_end.js');
 describe('Linked List Kth From End Module', () => {
 
   it('POL: undefined', () => {
-
-    const actual = new linkedList(2);
-    expect(actual).toBeUndefined();
+    const actual = new linkedList();
+    expect(actual.length).toBe(0);
   });
+  it('INSERT: from 2nd to last in linked list', () => {
+    const expected = 3;
+    
+    const newNode = new linkedList();
+    //get some nodes in the link list to start out with
+    const newAppend = newNode.append(2);
+    const newAppend1 = newNode.append(3);
+    const newAppend2 = newNode.append(4);
+    const newAppend3 = newNode.append(4);
+    
+    //entering the position 
+    const kthValue = newNode.find(2);
+    expect(kthValue).toBe(expected);
+  });
+
 });
