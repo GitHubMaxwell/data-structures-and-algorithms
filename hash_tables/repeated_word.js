0dbd771125df66337a8dd233291e36c98065079b
Maxwell Rediker <maxwell.rediker@gmail.com>
2018-11-01 13:21:12 -0700

    fixed indentation eslint erros

--- hash_tables/repeated_word.js
+++ hash_tables/repeated_word.js
@@ -1,17 +1,17 @@
 
 function repeatedWord(str) {
-    // let arr = str.split(' ')
-    let arr = str.split(/[\., ]/)
-    let obj = {}
+  // let arr = str.split(' ');
+  let arr = str.split(/[\., ]/);
+  let obj = {};
 
-    for(let i = 0 ; i < arr.length ; i++) {
-        if(!obj[arr[i]]) {
-          obj[arr[i]] = 1;
-        } else {
-          return arr[i];
-        }
-      }
-    return 'There are no duplicate words in this string.';
+  for (let i = 0; i < arr.length; i++) {
+    if (!obj[arr[i]]) {
+      obj[arr[i]] = 1;
+    } else {
+      return arr[i];
+    }
+  }
+  return 'There are no duplicate words in this string.';
 }
 
 module.exports = repeatedWord;
